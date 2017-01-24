85e0185b08708e25f39a7b6c0d57aa8ed903930c
Quentin Dufour <quentin@dufour.tk>
2017-01-24 11:54:22 +0100

    Add lisp code for euler 06 and 07 + update cheatsheet

---
+++ 06/mazeltov.lisp
@@ -0,0 +1,21 @@
+(defun square-sum (n)
+  (cond
+    ((<= n 0) 0)
+    (t (+
+        (square-sum (- n 1))
+        (* n n)
+))))
+
+(defun sum (n)
+  (cond
+    ((<= n 0) 0)
+    (t (+
+        (sum (- n 1))
+        n
+))))
+
+(defun sum-square (n)
+  (* (sum n) (sum n))
+)
+
+(write (- (sum-square 100) (square-sum 100)))
