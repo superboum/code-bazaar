bcb75acd9879b80e314a35330e4bb23d6980896a
Quentin Dufour <quentin@dufour.tk>
2017-08-06 10:19:42 +0200

    Solution to 08

---
+++ 08/main.lisp
@@ -0,0 +1,25 @@
+#!/usr/bin/sbcl --script
+
+(defun extract (l s)
+  (cond
+    ((eq s 0) '())
+    ((eq l '()) nil)
+    (t (cons (first l) (extract (rest l) (- s 1))))
+))
+
+(defun compute (adjacent greatest l)
+  (let ((factors (extract l adjacent)))
+    (cond
+      ((eq (last factors) nil) greatest)
+      ((> (reduce #'* factors) greatest) (compute adjacent (reduce #'* factors) (rest l)))
+      (t (compute adjacent greatest (rest l)))
+)))
+
+(defun parse ()
+  (let ((line (read-line *standard-input* nil)))
+    (cond
+      ((eq line nil) '())
+      (t (append (map 'list #'digit-char-p (coerce line 'list)) (parse)))
+)))
+
+(print (compute 13 0 (parse)))
