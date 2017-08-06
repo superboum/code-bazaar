8d84a3947f7555c23a028d34d748919d0510e220
Quentin Dufour <quentin@dufour.tk>
2017-08-06 15:42:22 +0200

    Add a stop to 10

--- 10/main.lisp
+++ 10/main.lisp
@@ -1,17 +1,18 @@
 #!/usr/bin/sbcl --script
 
-(defun can-be-divided (n l)
+(defun can-be-divided (n l stop)
   (cond
+    ((> stop n) nil)
     ((eq l nil) nil)
     ((eq (mod n (car l)) 0) (car l))
-    (t (can-be-divided n (cdr l)))
+    (t (can-be-divided n (cdr l) stop))
   )
 )
 
 (defun compute-primes (iterator limit primes)
   (cond
     ((>= iterator limit) primes)
-    ((can-be-divided iterator primes) (compute-primes (+ 2 iterator) limit primes))
+    ((can-be-divided iterator primes (isqrt iterator)) (compute-primes (+ 2 iterator) limit primes))
     (t (compute-primes (+ 2 iterator) limit (append primes (list iterator))))
 ))
 
