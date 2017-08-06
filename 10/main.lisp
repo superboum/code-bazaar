3d9e3adc5cc2cc22ce32a573b589509fccc38750
Quentin Dufour <quentin@dufour.tk>
2017-08-06 12:39:33 +0200

    Add solution for 10

---
+++ 10/main.lisp
@@ -0,0 +1,18 @@
+#!/usr/bin/sbcl --script
+
+(defun can-be-divided (n l)
+  (cond
+    ((eq l nil) nil)
+    ((eq (mod n (car l)) 0) (car l))
+    (t (can-be-divided n (cdr l)))
+  )
+)
+
+(defun compute-primes (iterator limit primes)
+  (cond
+    ((>= iterator limit) primes)
+    ((can-be-divided iterator primes) (compute-primes (+ 2 iterator) limit primes))
+    (t (compute-primes (+ 2 iterator) limit (cons iterator primes)))
+))
+
+(print (reduce #'+ (compute-primes 3 2000000 '(2))))
