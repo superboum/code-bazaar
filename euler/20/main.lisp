d84d51ddd5c8f20335ee9b20016e9c7a62cc91c2
Quentin Dufour <quentin@dufour.io>
2017-09-25 16:15:24 +0200

    Add euler 20

---
+++ euler/20/main.lisp
@@ -0,0 +1,13 @@
+(defun factorial (n)
+  (cond
+    ((eq n 0) 1)
+    (t (* n (factorial (- n 1))))
+))
+
+(defun add_digits (n)
+  (cond
+    ((eq n 0) 0)
+    (t (+ (mod n 10) (add_digits (floor n 10))))
+))
+
+(print (add_digits (factorial 100)))
