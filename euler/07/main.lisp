146716db3f49737794d5e090a9dfccef6fcf4d35
Quentin Dufour <quentin@dufour.io>
2017-09-30 09:35:38 +0200

    Improve 07 ompute time

--- euler/07/main.lisp
+++ euler/07/main.lisp
@@ -1,17 +1,18 @@
 #!/usr/bin/sbcl --script
 
-(defun can-be-divided (n l)
+(defun can-be-divided (n l stop)
   (cond
+    ((> (first l) stop) nil)
     ((eq l nil) nil)
     ((eq (mod n (car l)) 0) t)
-    (t (can-be-divided n (cdr l)))
+    (t (can-be-divided n (cdr l) stop))
 ))
 
 (defun prime (selected_number primes_list limit)
   (cond
     ((>= (list-length primes_list) limit) primes_list)
-    ((can-be-divided selected_number primes_list) (prime (+ selected_number 1) primes_list limit))
-    (t (prime (+ 1 selected_number) (append primes_list (list selected_number)) limit))
+    ((can-be-divided selected_number primes_list (isqrt selected_number)) (prime (+ selected_number 2) primes_list limit))
+    (t (prime (+ 2 selected_number) (append primes_list (list selected_number)) limit))
 ))
 
 (write (first (last (prime 3 '(2) 10001))))
