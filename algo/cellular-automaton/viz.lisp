96cbeda2d14c0523db4039db6c2bf9947d9d9167
Quentin Dufour <quentin@dufour.tk>
2017-12-09 15:20:26 +0100

    Switch between different starting positions

--- algo/cellular-automaton/viz.lisp
+++ algo/cellular-automaton/viz.lisp
@@ -9,8 +9,14 @@
    (width 1700)
    (height 900)
    (cell-size 5)
-   (config "config/spacefiller1_106.lif")
-   (cell-list (with-open-file (stream config) (parse-life106 stream)))
+   (configs '(
+     "config/spacefiller1_106.lif"
+     "config/puffer.lif"
+     "config/gosperglidergun_106.lif"
+     "config/bigun_106.lif"
+   ))
+   (config-pointer 0)
+   (cell-list '())
   )
   (map
     'list
@@ -23,5 +29,13 @@
     cell-list)
   (setq cell-list (one-turn cell-list))
 )
+
+(defmethod kit.sdl2:mousebutton-event ((window cellular-automaton) state ts b x y)
+  (cond
+    ((eq state :mousebuttondown)
+      (with-slots (cell-list configs config-pointer) window
+        (setq cell-list (with-open-file (stream (nth config-pointer configs)) (parse-life106 stream)))
+        (setq config-pointer (mod (+ config-pointer 1) (length configs)))
+))))
 
 (make-instance 'cellular-automaton)
