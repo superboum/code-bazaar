39a5107fbdadf213013740ebd2ef52c39721177c
Quentin Dufour <quentin@dufour.tk>
2017-12-12 12:42:10 +0100

    Create a main function, might export it one day in an exec

--- algo/cellular-automaton/run.lisp
+++ algo/cellular-automaton/run.lisp
@@ -1,4 +1,9 @@
 (push (make-pathname :directory (pathname-directory *load-truename*)) asdf:*central-registry*)
 (asdf:load-system 'cellular-automaton)
-(make-instance 'cellular-automaton:cellular-automaton)
+
+(defun main () (make-instance 'cellular-automaton:cellular-automaton))
+
+(main)
+; Broken for now
+;(sb-ext:save-lisp-and-die "my_binary" :executable t :toplevel 'main)
 
