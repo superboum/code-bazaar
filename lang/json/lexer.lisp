db2b756fe502bb6504432d1fc5379c1e8a1ce0a2
Quentin Dufour <quentin@dufour.tk>
2017-12-19 14:56:53 +0100

    Add a first JSON lexer

---
+++ lang/json/lexer.lisp
@@ -0,0 +1,51 @@
+; read-char, peek-char, unread-char
+
+(defun parse-float (s)
+  (with-input-from-string (in s)
+    (read in)
+))
+
+(defun is-digit (s)
+  (or (digit-char-p s) (eq #\. s) (eq #\- s))
+)
+
+(defun get-digit (start)
+  (labels (
+    (read-digit (s)
+      (cond
+        ((is-digit s) (cons s (read-digit (read-char))))
+        (t (unread-char s) nil)
+    ))
+  )
+  (list 'number (parse-float (coerce (read-digit start) 'string)))
+))
+
+(defun get-string ()
+  (labels (
+    (read-string ()
+      (let* ((s (read-char)))
+        (cond
+          ((eq #\" s) nil)
+          (t (cons s (read-string)))
+    )))
+  )
+  (list 'string (coerce (read-string) 'string))
+))
+
+(defun read-next ()
+  (let* ((s (read-char *standard-input* nil)))
+    (cond
+      ((eq nil s) nil)
+      ((is-digit s) (cons (get-digit s) (read-next)))
+      ((eq #\" s) (cons (get-string) (read-next)))
+      ((eq #\{ s) (cons (list 'start_brace) (read-next)))
+      ((eq #\} s) (cons (list 'end_brace) (read-next)))
+      ((eq #\[ s) (cons (list 'start_brack) (read-next)))
+      ((eq #\] s) (cons (list 'end_brack) (read-next)))
+      ((eq #\, s) (cons (list 'comma) (read-next)))
+      ((eq #\: s) (cons (list 'colon) (read-next)))
+      ((some (lambda (x) (eq x s)) (list #\space #\linefeed #\return #\tab)) (read-next))
+      (t (list 'error))
+)))
+
+(print (read-next))
