e620c31e50fde2e66422de41e95182656885966a
Quentin <quentin@deuxfleurs.fr>
2019-11-11 11:39:16 +0100

    compute rank

--- distributed/kademlia.js
+++ distributed/kademlia.js
@@ -5,20 +5,34 @@ const id_size = 32
 const timeout = 2000
 
 const kbuckets = Array.from({length: id_size}, () => [])
-const xorbuf = (b1, b2) => b1.map((v,i) => v ^ b2[i])
+const zipwith = (x, y, cb) => x.map((cur, idx) => cb(cur, y[idx]))
+const xorbuf = (b1, b2) => zipwith(b1, b2, (v1, v2) => v1 ^ v2) 
+const rank = (b, c) => {
+  if (b.length == 0) return c
+  const array_shift = parseInt(c / 8)
+  const byte_shift = 7 - (c % 8)
+  const m = b[array_shift] & (1 << byte_shift)
+  if (!m) return c
+  return rank(b, c+1)
+}
+
+const kbuckets_push = (nodeid) => {
+
+}
+
 const pending_requests = {}
 const handle_rpc = {
   res: (fd, msg, meta) => {
-    if (!pending_requests[msg.uid]) {
-      console.error(`No pending request for UID ${msg.uid}`)
+    if (!pending_requests[msg.msg_id]) {
+      console.error(`No pending request for UID ${msg.msg_id}`)
       return 
     }
-    clearTimeout(pending_requests[msg.uid].timeout);
-    pending_requests[msg.uid].resolve([fd,msg,meta])
-    delete pending_requests[msg.uid]
+    clearTimeout(pending_requests[msg.msg_id].timeout);
+    pending_requests[msg.msg_id].resolve([fd,msg,meta])
+    delete pending_requests[msg.msg_id]
   },
   ping: (fd, msg, meta) => {
-    const res = JSON.stringify({uid: msg.uid, action: 'res'})
+    const res = JSON.stringify({msg_id: msg.msg_id, action: 'res'})
     fd.send(res, meta.port, meta.ip, err => err ? console.error(err) : null)
   },
   find_node: (fd, msg, meta) => null,
@@ -32,8 +46,13 @@ const get_id = () => new Promise((resolve, reject) =>
   crypto.randomBytes(id_size, (err, buf) => err ? reject(err) : resolve(buf)))
 
 const check_rpc_msg_format = msg => {
-  if (!msg.uid) {
-    console.error("Message has no UUID", msg)
+  if (!msg.msg_id) {
+    console.error("Message has no message UUID", msg)
+    return false
+  }
+
+  if (!msg.emitter_id) {
+    console.error("Message has no emitter UUID", msg)
     return false
   }
 
@@ -46,8 +65,8 @@ const check_rpc_msg_format = msg => {
 }
 
 const rpc = (fd, ip, port, msg) => new Promise((resolve, reject) => {
-  const timer = setTimeout(() => delete pending_requests[msg.uid], 2000)
-  pending_requests[msg.uid] = { timeout: timer, resolve: resolve }
+  const timer = setTimeout(() => delete pending_requests[msg.msg_id], 2000)
+  pending_requests[msg.msg_id] = { timeout: timer, resolve: resolve }
   fd.send(JSON.stringify(msg), port, ip, err => err ? reject(err) : null)
 })
 
@@ -76,13 +95,14 @@ const start_network = port => new Promise((resolve, reject) => {
 get_id()
   .then(buf => {
     nodeid = buf
+    console.log(xorbuf(buf, buf))
     console.log(`node id is ${buf.toString('hex')}`)
     return start_network(process.env['KAD_PORT'])
   })
   .then(udpfd => {
     addr = udpfd.address()
     console.log(`node listening on ${addr.address}:${addr.port}`)
-    return rpc(udpfd, '127.0.0.1', addr.port, {uid: 'a', action: 'ping'})
+    return rpc(udpfd, '127.0.0.1', addr.port, {msg_id: 'a', action: 'ping'})
   })
   .then(([fd, msg, meta]) => {
     console.log('Ping success')
