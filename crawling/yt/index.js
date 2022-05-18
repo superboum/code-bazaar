355fe95a6cd6837989075bcd985b1cb5c6170ecd
Quentin Dufour <quentin@deuxfleurs.fr>
2022-05-18 12:47:57 +0200

    Improve output

--- crawling/yt/index.js
+++ crawling/yt/index.js
@@ -5,7 +5,7 @@ const parseString = require('xml2js').parseString;
 const fs = require('fs/promises');
 
 
-// Run console.log(JSON.stringify(Array.from(document.querySelectorAll("ytd-grid-video-renderer a#video-title[href]").entries()).map(e => e[1].href.match(/watch\?v=(.*)$/)).filter(e => e).map(e => e[1])))
+// Run console.log(JSON.stringify(Array.from(document.querySelectorAll("ytd-grid-video-renderer a#video-title[href]").entries()).map(e => e[1].href.match(/([a-zA-Z0-9_-]+)$/)).filter(e => e).map(e => e[1])))
 const video_set = require("./video_set.json");
 console.log("number of video fetched in the timeline:", video_set.length);
 
@@ -16,31 +16,33 @@ const xml = (cnt) => new Promise((resolve, reject) => parseString(cnt, (err, res
   const raw = await fd.readFile({encoding: "utf-8"});
   fd.close()
 
-  const channelRss = raw.split("\n").map(l => l.split(",")[0]).slice(1).filter(i => i != "").map(id => `https://www.youtube.com/feeds/videos.xml?channel_id=${id}`);
-  console.log("number of channels to fetch:", channelRss.length)
+  const channels = raw.split("\n").slice(1).map(l => l.split(",",3)).filter(([id, url, name]) => id)
+  const idToName = channels.reduce((acc, [id, url, name]) => { acc[id] = name ; return acc }, {})
+  console.log("number of channels to fetch:", channels.length)
 
   const channelContent = await Promise
-    .map(channelRss, l => {
+    .map(channels, ([id, url, name]) => {
+	    const l = `https://www.youtube.com/feeds/videos.xml?channel_id=${id}`;
 	    console.log(`fetching ${l}`)
-	    return axios.get(l).catch(e => { console.error(`axios failed on ${l}`); return null })
+	    return axios.get(l)
+	            .then(c => xml(c.data))
+		    .then(v => {
+                      try {
+                        return v.feed.entry.map(e => [e.published[0], e['yt:videoId'][0], id])
+                      } catch (error) {
+                        console.error(v, error);
+	                return []
+                      }
+		    })
+		    .catch(e => { console.error(`failure on ${id}`); return [] })
     }, {concurrency: 1})
-    .map(c => c ? xml(c.data) : null)
-    .map(v => {
-      if (!v) return []
-      try {
-        return v.feed.entry.map(e => [e.published[0], e['yt:videoId'][0]])
-      } catch (error) {
-        console.error(v, error);
-	return []
-      }
-    })
 
   const answer = channelContent
     .flat()
     .sort()
-    .map(([date, id]) => [date, id, video_set.includes(id)])
+    .map(([date, id, c]) => [date, id, idToName[c], video_set.includes(id)])
 
-  answer.forEach(([date, id, r]) => console.log(`${date} ${id} ${r ? '✅' : '❌'}`))
+  answer.forEach(([date, id, c, r]) => console.log(`${date} | ${c} | ${id} | ${r ? '✅' : '❌'}`))
   
 })()
 
