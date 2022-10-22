0fe32c0f29a60029c57faa95b059f2000dc01448
Quentin Dufour <quentin@deuxfleurs.fr>
2022-10-22 19:58:06 +0200

    Close to perfect calendar!

--- lang/scheme/calendar.scm
+++ lang/scheme/calendar.scm
@@ -58,31 +58,38 @@
       ((= (last-day-month date) (date-day lday)) (list week))
       (#t (cons week (gen-month-per-week (date-add-days lday 1)))))))
 
-(define cal-pad '("     " "     " "     "))
+(define cal-pad '("     " "     "))
 (define (cal-day-cell-base date)
   (list
     (format " ————")
-    (format "| ~2,'0d " (date-day date))
-    (format " ————")))
+    (format "| ~2,'0d " (date-day date))))
+    ;(format " ————")))
 
-(define (cal-day-cell-end week)
+(define (fill-right week)
   (let ([rweek (reverse week)])
-    (let-values ([(top mid bot) (apply values (car rweek))])
+    (let-values ([(top mid) (apply values (car rweek))])
       (reverse
         (cons
           (list
             (format "~a ~%" top)
-            (format "~a|~%" mid)
-            (format "~a ~%" bot))
+            (format "~a|~%" mid))
+            ;(format "~a ~%" bot))
           (cdr rweek))))))
 
+(define (fill-bottom month)
+  (let ([rmonth (reverse month)])
+    (reverse (cons (map (lambda (_) (list " ————")) (car rmonth)) rmonth))))
+
 (define (build-cells fdm)
-  (map (lambda (w) (cal-day-cell-end (map (lambda (d) (cal-day-cell-base d)) w))) (gen-month-per-week fdm)))
+  (fill-bottom
+    (map 
+      (lambda (w) (fill-right (map (lambda (d) (cal-day-cell-base d)) w))) 
+      (gen-month-per-week fdm))))
 
 (define (build-cells-with-pads fdm)
   (let ([days (build-cells fdm)]
-        [pads (map (lambda (cnt) cal-pad) (iota (weekday-fr fdm)))])
-    (cons (append pads (car days)) (cdr days))))
+        [pad-first (map (lambda (cnt) cal-pad) (iota (weekday-fr fdm)))])
+    (cons (append pad-first (car days)) (cdr days))))
 
 ;(define (build-cal-inner fdm)
 ;  (build-cell-with-pads fdm)
