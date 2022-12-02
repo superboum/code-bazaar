91503660f61d45c55980c066a182ce91a13424fd
Quentin Dufour <quentin@deuxfleurs.fr>
2022-12-02 21:45:05 +0100

    got day 1, part 1

--- aoc-2022/a1_1.scm
+++ aoc-2022/a1_1.scm
@@ -1,1 +1,15 @@
-(read)
+(define (collect)
+  (let ([line (get-line (current-input-port))])
+    (cond
+      ((eof-object? line) '())
+      (#t (cons line (collect))))))
+
+(define (aggregate cal-max cal-cur)
+  (let ([food (get-line (current-input-port))])
+    (cond
+      ((eof-object? food) (max cal-max cal-cur))
+      ((eq? food "") (aggregate (max cal-max cal-cur) 0))
+      (#t (aggregate cal-max (+ cal-cur (string->number food))))
+)))
+
+(format #t "~a" (aggregate 0 0))
