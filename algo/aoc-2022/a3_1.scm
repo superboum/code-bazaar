7f26ec96874c815f99ca9c91b2ea4580c969dbaf
Quentin Dufour <quentin@deuxfleurs.fr>
2022-12-03 11:23:34 +0100

    day 3 part 1

--- algo/aoc-2022/a3_1.scm
+++ algo/aoc-2022/a3_1.scm
@@ -6,20 +6,55 @@
     (bytevector-u8-set! 
       bv
       idx
-      (logbit1 shift (bytevector-u8-ref bv idx))
-)))
+      (logbit1 shift (bytevector-u8-ref bv idx)))
+    bv
+))
 (define (ascii-set-in? bv c) 
   (let-values ([(idx shift) (div-and-mod (char->integer c) 8)])
     (logbit? shift (bytevector-u8-ref bv idx))))
+(define (ascii-set-clear! bv) (bytevector-fill! bv 0) bv)
+
+; -> s: string
+; <- ascii-set
+(define (string->ascii-set s)
+  (let ([as (make-ascii-set)])
+    (map (lambda (c) (ascii-set-add! as c)) (string->list s))
+    as))
+
+; -> as: ascii-set, lc: list(char)
+; <- char
+(define (ascii-set-find as lc)
+  (cond
+    ((null? lc) #\nul)
+    ((ascii-set-in? as (car lc)) (car lc))
+    (#t (ascii-set-find as (cdr lc)))
+))
+
+; -> sl: string - the full line from the challenge
+; <- char - the redundant letter
+(define (find-redundant sl)
+  (let* ([len (string-length sl)]
+         [split (div len 2)])
+    (ascii-set-find
+      (string->ascii-set (substring sl 0 split))
+      (string->list (substring sl split len)))
+))
+
+; -> c: char - an ascii char
+; <- integer - the integer coding asked by the challenge
+(define (letter-coding c)
+  (cond
+    ((char<=? #\A c #\Z) (- (char->integer c) (char->integer #\A) -27))
+    ((char<=? #\a c #\z) (- (char->integer c) (char->integer #\a) -1))
+    (#t 0)
+))
 
 
-
-(define (result)
+(define (result acc)
   (let ([line (get-line (current-input-port))])
     (cond
-      ((eof-object? line) #t)
-      (#t
-        (format #t "~a~%" line)
-        (result)))))
+      ((eof-object? line) acc)
+      (#t (result (+ acc (letter-coding (find-redundant line)))))
+)))
 
-;(result)
+(format #t "~a" (result 0))
