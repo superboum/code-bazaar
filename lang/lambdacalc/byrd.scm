aa9dec827d61d270d7830cce9fca28d329f16175
Quentin Dufour <quentin@deuxfleurs.fr>
2024-09-02 12:29:21 +0200

    fix function, boolean works?!

--- lang/lambdacalc/byrd.scm
+++ lang/lambdacalc/byrd.scm
@@ -1,10 +1,10 @@
+;-- interpreter
 (define (eval-expr expr env)
   (cond
-    ((number? expr) expr)
     ((symbol? expr) (env expr))
-    ((eq? (car expr) 'lambda) 
+    ((eq? (car expr) 'f) 
       (let ([arg (cadr expr)]
-            [body (cadr expr)])
+            [body (caddr expr)])
       (lambda (y) (eval-expr body 
         (lambda (r) (if (eq? r arg) y (env r)))))))
     (#t
@@ -12,3 +12,12 @@
        (eval-expr (cadr expr) env) ; operand
     ))
 ))
+
+;-- wrappers
+(define (empty-env x) (raise 'variable-unbound))
+(define (lcalc-bool expr)
+  ;(((eval-expr expr empty-env) #t) #f))
+  (eval-expr expr empty-env))
+
+(define (lcacl-num expr)
+  ((eval-expr expr empty-env) (lambda (v) (+ v 1)) 0))
