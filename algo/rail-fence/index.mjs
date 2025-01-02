6709d3eaa3ae1a5d67bf7ed7f105caa6807373d2
Quentin Dufour <quentin@deuxfleurs.fr>
2025-01-02 08:11:20 +0100

    decale exec gen

--- algo/rail-fence/index.mjs
+++ algo/rail-fence/index.mjs
@@ -85,6 +85,6 @@ const gen = () => {
   const ret4 = dec.decode(ret3);
   dr(ret4);
 }
-gen();
+document.getElementById('alphabet').onload = gen;
 document.getElementById('gen').addEventListener('click', gen);
   
