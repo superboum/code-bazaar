beba59ae009a864a206c55a5cfa9c5bd560648b9
Quentin Dufour <quentin@deuxfleurs.fr>
2025-08-15 14:09:56 +0200

    some form of tokenizer

--- algo/markov/markov.scm
+++ algo/markov/markov.scm
@@ -1,3 +1,34 @@
+(define (find-eot-idx line-part bidx eidx specialization)
+  (cond
+    ((eq? specialization 'punct) (values specialization bidx eidx))
+    ((= eidx (string-length line-part)) (values specialization bidx eidx))
+    (#t (let ([curchar (string-ref line-part eidx)])
+	  (cond
+	    ((and (eq? specialization 'none) (char-whitespace? curchar))
+	     (find-eot-idx line-part (+ bidx 1) (+ eidx 1) 'none))
+	    ((and 
+	       (or (eq? specialization 'none) (eq? specialization 'word))
+	       (or (char-alphabetic? curchar) (char-numeric? curchar)))
+	     (find-eot-idx line-part bidx (+ eidx 1) 'word))
+	    ((eq? specialization 'none) 
+	     (values 'punct bidx (+ eidx 1)))
+	    (#t (values specialization bidx eidx))
+     )))
+))
+
+; First naive token implementation
+(define (extract-token line-part)
+  (cond
+    ((= (string-length line-part) 0) '())
+    (#t (let*-values 
+	  ([(spec bidx eidx) (find-eot-idx line-part 0 0 'none)])
+	  (cons
+	    (substring line-part bidx eidx)
+	    (extract-token (substring line-part eidx (string-length line-part)))
+    )))
+))
+
+
 (define (line-counter line acc) (+ acc 1))
 
 ; unroll
