fbeaff24a02c17095f4d30d684548a9f53ad0b0f
Quentin Dufour <quentin@deuxfleurs.fr>
2025-12-27 16:11:55 +0100

    print "hello world"

--- system/learnix/src/main.rs
+++ system/learnix/src/main.rs
@@ -1,11 +1,30 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
+
 #[unsafe(no_mangle)]
 fn main() {
+    let msg = b"Hello, World!";
+    for &ch in msg {
+        unsafe {
+            asm!(
+                "mov ah, 0x0E",   // INT 10h function to print a char
+                "mov al, {0}",    // The input ASCII char
+                "int 0x10",       // Call the BIOS Interrupt Function
+                                  // --- settings ---
+                in(reg_byte) ch,  // {0} Will become the register with the char
+                out("ax") _,      // Lock the 'ax' as output reg, so it won't be used elsewhere
+            );
+        }
+    }
+
+    unsafe {
+        asm!("hlt"); // Halt the system
+    }
 }
 
 #[panic_handler]
 pub fn panic_handler(_: &core::panic::PanicInfo) -> ! {
-  loop {}
+    loop {}
 }
