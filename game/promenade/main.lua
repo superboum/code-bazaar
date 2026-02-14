19b1f02474d9e740d9ff3d93dad11226b7670919
Quentin Dufour <quentin@deuxfleurs.fr>
2026-02-14 20:19:41 +0100

    implement run

--- game/promenade/main.lua
+++ game/promenade/main.lua
@@ -8,9 +8,10 @@ CatAnimation = {
   current_anim = nil,
   orientation = 1, -- 1 = right, -1 = left
 
-  speed = 100,
+  walk_speed = 100,
+  run_speed = 200,
 
-  pos_x = 0,
+  pos_x = 32,
   pos_y = 400,
 
   -- quad is computed later
@@ -33,7 +34,6 @@ function CatAnimation:load()
 
     for anim_name, anim_data in pairs(CatAnimation.spritemap) do
         anim_idx = 1
-	print(anim_name)
 	while anim_idx <= anim_data.len do
 	    anim_data.quads[anim_idx] = love.graphics.newQuad(
 		    (anim_idx-1)*spr_sz, -- col (x), here frame selection for anim
@@ -53,7 +53,7 @@ function CatAnimation.tick(dt)
     CatAnimation.current_frame = math.floor(CatAnimation.current_time / CatAnimation.current_anim.dur * #CatAnimation.current_anim.quads) + 1
 end
 
-function CatAnimation.move(dt)
+function CatAnimation.walk(dt)
     if CatAnimation.current_anim ~= CatAnimation.spritemap.walk then
 	CatAnimation.current_anim = CatAnimation.spritemap.walk
 	CatAnimation.current_time = 0
@@ -66,12 +66,44 @@ function CatAnimation.move(dt)
 	CatAnimation.orientation = 1
     end
 
-    CatAnimation.pos_x = CatAnimation.pos_x + dt * CatAnimation.speed
+    CatAnimation.pos_x = CatAnimation.pos_x + dt * CatAnimation.walk_speed
+end
+
+function CatAnimation.run(dt)
+    if CatAnimation.current_anim ~= CatAnimation.spritemap.run then
+	CatAnimation.current_anim = CatAnimation.spritemap.run
+	CatAnimation.current_time = 0
+	CatAnimation.current_frame = 1
+    end
+
+    if dt < 0 then
+	CatAnimation.orientation = -1
+    else
+	CatAnimation.orientation = 1
+    end
+
+    CatAnimation.pos_x = CatAnimation.pos_x + dt * CatAnimation.run_speed
 end
 
 function CatAnimation.rest()
-    if CatAnimation.current_anim ~= CatAnimation.spritemap.clean then
-	CatAnimation.current_anim = CatAnimation.spritemap.clean
+    if CatAnimation.current_anim ~= CatAnimation.spritemap.idle_face then
+	CatAnimation.current_anim = CatAnimation.spritemap.idle_face
+	CatAnimation.current_time = 0
+	CatAnimation.current_frame = 1
+    end
+end
+
+function CatAnimation.sleep()
+    if CatAnimation.current_anim ~= CatAnimation.spritemap.sleep then
+	CatAnimation.current_anim = CatAnimation.spritemap.sleep
+	CatAnimation.current_time = 0
+	CatAnimation.current_frame = 1
+    end
+end
+
+function CatAnimation.jump()
+    if CatAnimation.current_anim ~= CatAnimation.spritemap.jump then
+	CatAnimation.current_anim = CatAnimation.spritemap.jump
 	CatAnimation.current_time = 0
 	CatAnimation.current_frame = 1
     end
@@ -101,17 +133,27 @@ function love.update(dt)
     CatAnimation.tick(dt)
 
     if love.keyboard.isDown("right") then
-	CatAnimation.move(dt)
+	if love.keyboard.isDown("lshift") then
+	  CatAnimation.run(dt)
+	else
+	  CatAnimation.walk(dt)
+	end
     elseif love.keyboard.isDown("left") then
-	CatAnimation.move(-dt)
+	if love.keyboard.isDown("lshift") then
+	  CatAnimation.run(-dt)
+	else
+	  CatAnimation.walk(-dt)
+	end
+    elseif love.keyboard.isDown("down") then
+	CatAnimation.sleep()
+    elseif love.keyboard.isDown("up") then
+	CatAnimation.jump()
     else
 	CatAnimation.rest()
     end
 end
 
 function love.draw()
-    --love.graphics.rectangle("fill", x, 200, 50, 80)
-    --
     CatAnimation.draw()
     love.graphics.print("Hello World", 400, 300)
 end
