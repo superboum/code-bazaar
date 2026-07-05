942a2f3ee5f654ffafd5b883e0a93c8c9fbefe13
Quentin Dufour <quentin@deuxfleurs.fr>
2026-07-05 19:37:47 +0200

    split into 2 files, read json

--- internet/degrowth-games/index.mjs
+++ internet/degrowth-games/index.mjs
@@ -1,47 +1,12 @@
 import sqlite from 'node:sqlite';
+import { readFileSync } from 'node:fs';
+
+const games_appid_list_src = "https://github.com/jsnli/steamappidlist/raw/refs/heads/master/data/games_appid.json"
+// Queried API endpoint -> https://api.steampowered.com/IStoreService/GetAppList/v1/
+const games_appid_list_dst = "./games_appid.json"
+
 
 const database = new sqlite.DatabaseSync('steam.db');
 
-database.exec(`
-  CREATE TABLE IF NOT EXISTS games(
-    appid INTEGER,
-    last_appdetails_update INTEGER,
-    name TEXT,
-    description TEXT,
-    win_min_requirements TEXT,
-    win_reco_requirements TEXT,
-    mac_min_requirements TEXT,
-    mac_reco_requirements TEXT,
-    lin_min_requirements TEXT,
-    lin_reco_requirements TEXT,
-    release_date INTEGER,
-    recommendations INTEGER
-  ) STRICT;
-  CREATE UNIQUE INDEX IF NOT EXISTS idx_game_appid ON games(appid);
-
-  CREATE TABLE IF NOT EXISTS images(
-    appid INTEGER,
-    kind TEXT,
-    url TEXT,
-    FOREIGN KEY(appid) REFERENCES games(appid)
-  ) STRICT;
-  CREATE INDEX IF NOT EXISTS idx_images_appid_kind ON images(appid, kind);
-
-  CREATE TABLE IF NOT EXISTS tags(
-    id INTEGER PRIMARY KEY,
-    kind TEXT,
-    steam_id INTEGER,
-    name TEXT
-  ) STRICT;
-  CREATE UNIQUE INDEX IF NOT EXISTS idx_tags_steam_ref ON tags(kind, steam_id);
-
-  CREATE TABLE IF NOT EXISTS games_tags(
-    tag_id INTEGER,
-    appid INTEGER,
-    FOREIGN KEY(appid) REFERENCES games(appid),
-    FOREIGN KEY(tag_id) REFERENCES tags(id)
-  ) STRICT;
-  CREATE INDEX IF NOT EXISTS idx_game_to_tags ON games_tags(appid);
-  CREATE INDEX IF NOT EXISTS idx_tags_to_game ON games_tags(tag_id);
-  CREATE UNIQUE INDEX IF NOT EXISTS idx_game_tag_uniqueness ON games_tags(appid, tag_id);
-`);
+const list = JSON.parse(readFileSync(games_appid_list_dst));
+console.log(list[0]);
