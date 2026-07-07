165cc367ac96b2a605da602f04de85dff04a13da
Quentin Dufour <quentin@deuxfleurs.fr>
2026-07-07 11:58:56 +0200

    support multiple crawls instances

--- internet/degrowth-games/fetch.mjs
+++ internet/degrowth-games/fetch.mjs
@@ -1,5 +1,6 @@
 import { readFile, open } from 'node:fs/promises';
 import { setTimeout } from 'node:timers/promises';
+import { argv } from 'node:process';
 
 // Queried API endpoint -> https://api.steampowered.com/IStoreService/GetAppList/v1/
 const games_appid_list_src = "https://github.com/jsnli/steamappidlist/raw/refs/heads/master/data/games_appid.json"
@@ -15,6 +16,33 @@ export async function appdetails() {
   const list = JSON.parse(await readFile(games_appid_list_dst));
   console.log(`known games: ${list.length}`);
 
+  if (argv.length > 3) {
+    const past_crawls = argv.slice(3);
+    console.log(`Reading past crawls: ${past_crawls}`);
+    await past_crawls.reduce(async (promise, crawl_path) => {
+      await promise;
+      const past_fd = await open(crawl_path, 'r');
+      for await (const line of past_fd.readLines()) {
+        const many_appdetails = JSON.parse(line);
+	if (many_appdetails == null) continue;
+	Object.keys(many_appdetails)
+	.map(obj_key => parseInt(obj_key))
+        // We expect the appid to be at the beginning of the list
+	// We find its index
+	.map(appid => list.findIndex(elem => elem.appid === appid))
+	.forEach(appid_idx => {
+          if (appid_idx < 0) {
+	    //appid_idx is -1 if not found
+            return
+	  }
+	  // We add a fetched key to remember to skip it.
+	  list[appid_idx].fetched = true;
+	})
+      }
+      await past_fd.close();
+    }, Promise.resolve());
+  }
+
   const out = await open(games_appdetails_dst, 'a');
 
   await list.reduce(async (promise, appdesc) => {
@@ -22,6 +50,10 @@ export async function appdetails() {
     // ie. one request every 1.5 sec.
     await promise 
     const appid = appdesc.appid
+    if (appdesc.fetched) {
+      console.log(`skipping ${appid}, already fetched.`);
+      return
+    }
     console.log(`fetch ${appid}`);
     const appdetails_req = await fetch(games_appdetails_api + appid);
     const appdetails_json = await appdetails_req.json()
