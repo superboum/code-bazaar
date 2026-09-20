5fad4a9489e1b28a725760f0ede687eaaf5e53f0
Quentin Dufour <quentin@deuxfleurs.fr>
2026-09-20 18:52:19 +0200

    find git folder

--- system/gitoy/main.py
+++ system/gitoy/main.py
@@ -1,5 +1,22 @@
-def main():
-    print("Hello from gitoy!")
+from pathlib import Path
+
+class GitDirNotFound(Exception):
+    pass
+
+def find_git_folder(directory: Path|None = None) -> Path:
+    if directory is None:
+        directory = Path(".").absolute()
+
+    candidate = directory / Path(".git")
+    if candidate.is_dir():
+        return candidate
+    elif directory.parent == directory:
+        raise GitDirNotFound
+    else:
+        return find_git_folder(directory.parent)
+
+def main() -> None:
+    print(find_git_folder())
 
 
 if __name__ == "__main__":
