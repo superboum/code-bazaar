9d16cd61f4ce429df0877e9d61f9a23f1ccd9e27
Quentin Dufour <quentin@deuxfleurs.fr>
2026-09-20 20:49:54 +0200

    start adding references on git

--- system/gitoy/README.md
+++ system/gitoy/README.md
@@ -1,3 +1,12 @@
-## Try to understand git...
+## Trying to understand git...
+
+[Similarly to Seafile](https://git.deuxfleurs.fr/quentin/seafile_recovery), Git has a ref/commit/tree/blob approach to storing content.
+It is summarized by this picture extracted from chapter 10.3 of git internals:
+
+![git ref/commit/tree/blob model](./doc/git-data-model.png)
 
 
+## References
+ - Git Internals
+   - [10.2 Git Internals - Git Objects](https://git-scm.com/book/en/v2/Git-Internals-Git-Objects)
+   - [10.3 Git Internals - Git References](https://git-scm.com/book/en/v2/Git-Internals-Git-References)
--- system/gitoy/main.py
+++ system/gitoy/main.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import zlib
+import io
 
 class GitDirNotFound(Exception):
     pass
@@ -11,12 +13,24 @@ def find_git_folder(directory: Path|None = None) -> Path:
     if candidate.is_dir():
         return candidate
     elif directory.parent == directory:
-        raise GitDirNotFound
+        raise GitDirNotFound()
     else:
         return find_git_folder(directory.parent)
 
+def find_git_root_obj(git_dir: Path) -> Path:
+    # @FIXME will implement later
+    pass
+
+def extract_zlib_object(obj: Path) -> bytes:
+    return zlib.decompress(obj.read_bytes())
+
+def read_commit(commit: Path) -> str:
+    #@FIXME: there is a null terminated string first b'commit 481\x00'
+    return extract_zlib_object(commit).decode("utf8")
+
 def main() -> None:
-    print(find_git_folder())
+    #print(read_commit(Path("../../.git/objects/5f/ad4a9489e1b28a725760f0ede687eaaf5e53f0")))
+    print(extract_zlib_object(Path("../../.git/objects/a1/fb136e2b37492f59160ab8254be1101f9a7650")))
 
 
 if __name__ == "__main__":
